d04c5ab38dd617664a15bb40a8a6768f58f97949
Celso Kikuchi <celso.kikuchi@serpro.gov.br>
2021-05-03 14:32:00 -0300

    bug: Can't resolve 'ngx-bootstrap'

--- exercicios/exercicio09LifecycleHooks/src/app/app.module.ts
+++ exercicios/exercicio09LifecycleHooks/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { PeekABooParentComponent } from './peek-a-boo-parent.component'; // <-- importe o símbolo PeekABooComponent
 import { PeekABooComponent } from './peek-a-boo.component'; // <-- importe o símbolo PeekABooComponent
+import { AccordionModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AccordionModule.forRoot(),
     CommonModule,
     BrowserModule
   ],
